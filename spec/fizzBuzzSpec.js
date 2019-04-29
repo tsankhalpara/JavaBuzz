0584485b1a2feccfd7656fdc492b4289c32db23b
Tara Sankhalpara <tsankhalpara@mac.com>
2019-04-29 16:31:47 +0100

    run same tests for numbers divisible/not divisible by 5

--- spec/fizzBuzzSpec.js
+++ spec/fizzBuzzSpec.js
@@ -5,11 +5,17 @@ describe("FizzBuzz", function() {
     it ('divisible by 3', function() {
       expect(fizzbuzz.isDivisibleByThree(6)).toBe(true);
     });
+    it ('divisible by 5', function() {
+      expect(fizzbuzz.isDivisibleByFive(5)).toBe(true);
+    });
   });
 
   describe('knows when number is NOT', function() {
     it ('divisible by 3', function() {
       expect(fizzbuzz.isDivisibleByThree(8)).toBe(false);
+    });
+    it ('divisible by 5', function() {
+      expect(fizzbuzz.isDivisibleByFive(9)).toBe(false);
     });
   });
 
--- src/fizzBuzz.js
+++ src/fizzBuzz.js
@@ -3,3 +3,7 @@ var FizzBuzz = function() {};
 FizzBuzz.prototype.isDivisibleByThree = function(number) {
   return (number % 3 === 0);
 };
+
+FizzBuzz.prototype.isDivisibleByFive = function(number) {
+  return (number % 5 === 0);
+};
